ce5db48998d4997cf8eeba7c15a21fa300c1b67e
Jonny <j.ed.glossop@gmail.com>
2019-03-23 16:43:30 +0000

    Added methods for lazily reading input to allow for infinite streams!

--- src/Main.hs
+++ src/Main.hs
@@ -4,14 +4,39 @@ import Tokens
 
 import Tokens
 import System.Environment
+import Data.Char(digitToInt)
+
+
+--Reads in one line at a time and splits into seperate streams
+--Eg 3 5 4 -> [3, 5, 4]
+extractStreamLine :: String -> [Int]
+extractStreamLine (' ':rest) = [] ++ (extractStreamLine rest)
+extractStreamLine (num:rest) = [digitToInt num] ++ (extractStreamLine rest)
+extractStreamLine [] = []
+
+fetchStreamLine :: IO [Int]
+fetchStreamLine = do 
+    line <- getLine
+    putStrLn ("Fetched:" ++ line)
+    return (extractStreamLine line)
+
+--Reads in stream inputs and returns a list of int arrays
+--Eg 
+-- 3 5 4
+-- 2 3 1 -> [[3, 5, 4], [2, 3, 1], [6, 3, 7]]
+-- 6 3 7
+formStreams :: [IO [Int]]
+formStreams = fetchStreamLine:formStreams
 
 main :: IO ()
 main = do
     args <- getArgs
     source <- readFile (head args)
-    input <- getContents
-    
+    let inputStreams = formStreams
+
     let tokens = alexScanTokens source
     putStrLn (show tokens)
-    putStrLn (show input)
-
+    firstInputs <- (inputStreams!!1)
+    putStrLn (show firstInputs)
+    secondInputs <- (inputStreams!!2)
+    putStrLn (show secondInputs)
