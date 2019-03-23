aa6421b77c0c536c842c6ab7a1e9b9ff32630350
Jonny <j.ed.glossop@gmail.com>
2019-03-23 21:09:20 +0000

    Loads input fully at start but makes no assumptions with length. Caching was cool but pointless (streams have to be finite) and it is beyond the scope of this task.

--- src/Main.hs
+++ src/Main.hs
@@ -5,38 +5,42 @@ import Tokens
 import Tokens
 import System.Environment
 import Data.Char(digitToInt)
+import Data.List
 
+extractLines :: String -> String -> [String]
+extractLines ('\n':rest) building = building:(extractLines rest "")
+extractLines (c:rest) building = extractLines rest (building ++ [c])
+extractLines [] s = []
 
---Reads in one line at a time and splits into seperate streams
---Eg 3 5 4 -> [3, 5, 4]
-extractStreamLine :: String -> [Int]
-extractStreamLine (' ':rest) = [] ++ (extractStreamLine rest)
-extractStreamLine (num:rest) = [digitToInt num] ++ (extractStreamLine rest)
-extractStreamLine [] = []
+convertLine :: String -> String -> [Int]
+convertLine (' ':rest) building = (read building :: Int):(convertLine rest "")
+convertLine (s:"") building = [(read (s:building) :: Int)]
+convertLine (s:rest) building = convertLine rest (s:building)
 
-fetchStreamLine :: IO [Int]
-fetchStreamLine = do 
-    line <- getLine
-    putStrLn ("Fetched:" ++ line)
-    return (extractStreamLine line)
+numberise :: [String] -> [[Int]]
+numberise (line:rest) = (convertLine line ""):(numberise rest)
+numberise [] = []
 
 --Reads in stream inputs and returns a list of int arrays
 --Eg 
 -- 3 5 4
--- 2 3 1 -> [[3, 5, 4], [2, 3, 1], [6, 3, 7]]
+-- 2 3 1 -> [[3, 2, 6], [5, 3, 3], [4, 1, 7]]
 -- 6 3 7
-formStreams :: [IO [Int]]
-formStreams = fetchStreamLine:formStreams
+formStreams :: String -> [[Int]]
+formStreams input = transpose ( numberise (extractLines input "" ))
 
 main :: IO ()
 main = do
     args <- getArgs
     source <- readFile (head args)
-    let inputStreams = formStreams
+    input <- getContents
+    let inputStreams = formStreams input
 
-    let tokens = alexScanTokens source
-    putStrLn (show tokens)
-    firstInputs <- (inputStreams!!1)
+    putStrLn (show inputStreams)
+
+    --let tokens = alexScanTokens source
+    --putStrLn (show tokens)
+    let firstInputs = (inputStreams!!0)
     putStrLn (show firstInputs)
-    secondInputs <- (inputStreams!!2)
+    let secondInputs = (inputStreams!!2)
     putStrLn (show secondInputs)
